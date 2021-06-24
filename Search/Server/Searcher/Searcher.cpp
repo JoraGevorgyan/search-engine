73016e35060ca93c71c8e5f9598ae25d6513a869
jora <jora@ispras.ru>
2021-06-24 10:28:12 +0400

    code style change

--- Search/Server/Searcher/Searcher.cpp
+++ Search/Server/Searcher/Searcher.cpp
@@ -5,8 +5,8 @@
 #include "Searcher.hpp"
 
 Searcher::Searcher(const std::string& dbName, const std::string& dbServer,
-                  const std::string& dbUsername, const std::string& dbPassword,
-                  const unsigned long& dbPort, int maxResultCount)
+                   const std::string& dbUsername, const std::string& dbPassword,
+                   const unsigned long& dbPort, int maxResultCount)
         : connection(true), maxResultCount(maxResultCount)
 {
     this->connection.connect(dbName.c_str(), dbServer.c_str(), dbUsername.c_str(), dbPassword.c_str(), dbPort);
