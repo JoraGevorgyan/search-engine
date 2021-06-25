c52c7559c47cd6bf45b42c7e9be636fd9f222d5d
Jora Gevorgyan <jora@ispras.ru>
2021-06-25 23:52:20 +0400

    mark [[nodiscard]] for some methods and make them public

--- UI/UI/ConsoleUI.hpp
+++ UI/UI/ConsoleUI.hpp
@@ -10,10 +10,11 @@
 namespace ui {
 
 class ConsoleUI : public UI {
+public:
     void showMsg(const std::string& msg, bool err = false) const override;
     void showSearchResult(const web::json::value& result) const override;
-    std::string ask(const std::string& question) const override;
-    std::string askForSearch() const override;
+    [[nodiscard]] std::string ask(const std::string& question) const override;
+    [[nodiscard]] std::string askForSearch() const override;
 };
 } // namespace ui
 
