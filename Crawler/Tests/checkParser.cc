f7099d311b376fe5d6d620acadb100e053ddd781
Jora Gevorgyan <jora@ispras.ru>
2021-06-27 00:12:29 +0400

    rename .html from test file names

--- Crawler/Tests/checkParser.cc
+++ Crawler/Tests/checkParser.cc
@@ -15,56 +15,56 @@
 
 TEST(checkParser, fail_1)
 {
-	const auto html = readFile("./parser/valid.html");
+	const auto html = readFile("./parser/valid");
 	Parser parser(html, INVALID_URL);
 	EXPECT_EQ(parser.invalid(), true);
 }
 
 TEST(checkParser, fail_2)
 {
-	const auto html = readFile("./parser/invalid.html");
+	const auto html = readFile("./parser/invalid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getDescription().empty(), true);
 }
 
 TEST(checkParser, fail_3)
 {
-	const auto html = readFile("./parser/invalid.html");
+	const auto html = readFile("./parser/invalid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getTitle().empty(), true);
 }
 
 TEST(checkParser, fail_4)
 {
-	const auto html = readFile("./parser/invalid.html");
+	const auto html = readFile("./parser/invalid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getUrls().empty(), true);
 }
 
 TEST(checkParser, title)
 {
-	const auto html = readFile("./parser/valid.html");
+	const auto html = readFile("./parser/valid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getTitle(), EXPECTED_TITLE);
 }
 
 TEST(checkParser, desctiption)
 {
-	const auto html = readFile("./parser/valid.html");
+	const auto html = readFile("./parser/valid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getDescription(), EXPECTED_DESCRIPTION);
 }
 
 TEST(checkParser, content)
 {
-	const auto html = readFile("./parser/valid.html");
+	const auto html = readFile("./parser/valid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getContent().size(), EXPECTED_CONTENT_SIZE);
 }
 
 TEST(checkParser, urls)
 {
-	const auto html = readFile("./parser/valid.html");
+	const auto html = readFile("./parser/valid");
 	Parser parser(html, VALID_URL);
 	EXPECT_EQ(parser.getUrls().size(), EXPECTED_URLS_COUNT);
 }
